3142b30d229c8d29bf01a7b76fb2a38fc7923e38
Samuel <samugit.projects@gmail.com>
2023-12-05 15:25:41 -0300

    Aula13 Loop do jogo: versão 1

--- tictactoe/core/Game.java
+++ tictactoe/core/Game.java
@@ -1,5 +1,8 @@
 package tictactoe.core;
 
+import tictactoe.io.Input;
+import tictactoe.io.Output;
+
 @SuppressWarnings("ALL")
 public class Game {
 
@@ -8,8 +11,31 @@ public class Game {
 
     public void start() {
 
-        // teste impressão do tabuleiro
-        System.out.println(board);
+
+
+        // primeira versão do loop do jogo
+        while (true) {
+            Output.writeNewLine();
+            Output.write(board); // mostra o tabuleiro em branco
+            play(players.next()); // players.next é o proximo símbolo do jogador na jogada
+
+
+
+        }
+
+    }
+
+    private void play(Symbol symbol) { // método com validação só prossegue quando jogada válida
+        while (true){
+            try {
+                String play = Input.read(String.format("'%s' Play =>", symbol));
+                Coord coord = Coord.parse(play);
+                board.update(symbol, coord);
+                break;
+            } catch (RuntimeException e){
+                Output.write("ERROR: " + e.getMessage());
+            }
+        }
 
     }
 }
--- tictactoe/io/Input.java
+++ tictactoe/io/Input.java
@@ -9,7 +9,7 @@ public final class Input {
 
     public static String read(String message){ // ler informação do teclado, fica bloqueado até o usuário digitar a informação e dar um enter
         if (message != null) {
-            Output.write(message + " "); // alterado o write para não quebrar linha nesse caso
+            Output.write(message + " ", false); // alterado o write para não quebrar linha nesse caso
         }
 
         return new Scanner(System.in).nextLine(); // não fechar o Scanner pois fechará o System.in e não poderá ler dados do console
