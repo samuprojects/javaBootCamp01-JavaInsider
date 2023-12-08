52f3f9af50f16387d421391e84fc5e9e4ecbeefb
Samuel <samugit.projects@gmail.com>
2023-12-08 14:12:06 -0300

    Aula16 Entendendo a busca por sequências no tabuleiro

---
+++ theory/Search4SequencesOnTheBoard.java
@@ -0,0 +1,29 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class Search4SequencesOnTheBoard {
+
+    /*
+    *
+    *            |    |    |
+    *            v    v    v
+    *            0    1    2
+    * --> 0        |     |
+    *         _____|_____|______   - Row 1                 Find
+    * --> 1        |     |         - Row 2             |     |      |
+    *         _____|_____|______   - Row 3           Rows   Cols   Diags
+    * --> 2        |     |                           0 1 2  0 1 2    1 2
+    *              |     |         - Col 1
+    *                              - Col 2
+    *                              - Col 3
+    *
+    *                              - Diag 1
+    *                              - Diag 2
+    *
+    * o método findSequence deverá buscar padrões de sequencia dos símbolos dos jogadores na sequencia:
+    *  - das linhas;
+    *  - das colunas;
+    *  - e nas diagonais.
+    *
+    * */
+}
